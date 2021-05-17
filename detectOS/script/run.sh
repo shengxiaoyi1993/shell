762030b6d49d7da6dec05b67bca20db0acb4139f
shengxiaoyi <shengxiaoyi2014@icloud.com>
2021-05-17 15:55:58 +0800

    add

--- detectOS/script/run.sh
+++ detectOS/script/run.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+echo  "$(expr substr $(uname -s) 1 10)"
+
 if [[ "$(uname)" == "Darwin" ]]; then
 # Mac OS X 操作系统
   echo "mac"
@@ -7,7 +9,7 @@ if [[ "$(uname)" == "Darwin" ]]; then
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 # GNU/Linux操作系统
   echo "linux"
-elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]]; then
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
 # Windows NT操作系统
   echo "win"
 else
